1aa39996dd31190984c49af69347c83954d2a080
Alisher20070215 <140309349+Alisher20070215@users.noreply.github.com>
2023-08-25 20:39:36 +0600

    Create homework4.py

---
+++ homework4.py
@@ -0,0 +1,19 @@
+class People:
+    people = "people"
+
+    def __init__(self, name):
+        self.name = name
+
+class People2:
+    people = "people"
+
+    def __init__(self, age):
+        self.age = age
+
+class A:
+    def a(self):
+        print(self)
+
+class B:
+    def b(self):
+        print(self)
